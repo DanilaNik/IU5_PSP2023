76fd51dc5c79e51d86b35bd0c265ba2daac8dfdf
DanilaNik <d220203@mail.ru>
2023-05-31 22:21:08 +0300

    Add lab4

---
+++ lab5/components/product/index.js
@@ -0,0 +1,35 @@
+export class ProductComponent {
+    constructor(parent) {
+        this.parent = parent
+    }
+
+    getHTML(data) {
+        return (
+            `
+                <div class="card mb-3" style="width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="${data.photo_400_orig}" class="img-fluid" alt="картинка">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">${data.first_name} ${data.last_name}</h5>
+                            </div>
+                            <div>
+                                <p>День рождения: ${data.bdate}</p>
+                                <p>Город: ${data.city.title}</p>
+                                <p>Университет: ${data.university_name}</p>
+                                <p>Факультет: ${data.faculty_name}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `
+        )
+    }
+
+    render(data) {
+        const html = this.getHTML(data)
+        this.parent.insertAdjacentHTML('beforeend', html)
+    }
+}
